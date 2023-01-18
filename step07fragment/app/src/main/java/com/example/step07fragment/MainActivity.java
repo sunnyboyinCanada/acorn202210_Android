6ce2cd9599e721c31b918053bab8d5525b867bca
SunhyunKim <conestoga1214@gmail.com>
2023-01-18 14:24:43 +0900

    Step07 Fragment updated

---
+++ step07fragment/app/src/main/java/com/example/step07fragment/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.step07fragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity implements MyFragment.MyFragmentListener {
+    MyFragment mf1, mf2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+        //전개된 view 에는 MyFragment 객체가 2개 있다. 만일 해당 객체의 참조값이 액티비티에서 필요 하다면?
+
+        //FragmentManager 객체의 참조값을 얻어내서
+        FragmentManager fm=getSupportFragmentManager();
+        //해당객체의 메소드를 활용해서 프레그먼트의 참조값을 얻어낸다.
+        mf1=(MyFragment) fm.findFragmentById(R.id.fragment1);
+        mf2=(MyFragment) fm.findFragmentById(R.id.fragment2);
+
+        //만일 FragmentContainerView 의 참조값이 필요하다면 액티비티의 메소드를 이용해서 얻어낼수 있다.
+        FragmentContainerView container1=findViewById(R.id.fragment1);
+        FragmentContainerView container2=findViewById(R.id.fragment2);
+
+    }
+
+    @Override
+    public void sendMsg(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void resetFragment(View v){
+        mf1.reset();
+        mf2.reset();
+    }
+}
